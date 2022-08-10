5d40c3ab55ff841910e84618c30aeb1f53b2af85
Cole Maxwell <cole.maxwell@deltadentalsd.com>
2022-08-10 14:57:04 -0500

    Change model to have nested compensation

--- models/Employee.js
+++ models/Employee.js
@@ -1,18 +1,20 @@
 module.exports = class Employee {
-    constructor(row) {
-      this.employeeNumber = row.EMPNO;
-      this.firstName = row.FIRSTNME;
-      this.middleName = row.MIDINIT;
-      this.lastName = row.LASTNAME;
-      this.workDepartment = row.WORKDEPT;
-      this.phoneNumber = row.PHONENO;
-      this.hireDate = row.HIREDATE;
-      this.job = row.JOB;
-      this.educationLevel = row.EDLEVEL;
-      this.sex = row.SEX;
-      this.dateOfBirth = row.BIRTHDATE;
-      this.yearlySalary = row.SALARY;
-      this.yearlyBonus = row.BONUS;
-      this.yearlyCommission = row.COMM;
+  constructor(row) {
+    this.employeeNumber = row.EMPNO;
+    this.firstName = row.FIRSTNME;
+    this.middleName = row.MIDINIT;
+    this.lastName = row.LASTNAME;
+    this.workDepartment = row.WORKDEPT;
+    this.phoneNumber = row.PHONENO;
+    this.hireDate = row.HIREDATE;
+    this.job = row.JOB;
+    this.educationLevel = row.EDLEVEL;
+    this.sex = row.SEX;
+    this.dateOfBirth = row.BIRTHDATE;
+    this.compensation = {
+      yearlySalary: row.SALARY,
+      yearlyBonus: row.BONUS,
+      yearlyCommission: row.COMM,
     }
   }
+}

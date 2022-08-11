0dc2749f5a45a998ac67e792d26c8c13aa9039c2
Cole Maxwell <cole.maxwell@deltadentalsd.com>
2022-08-11 14:29:44 -0500

    Add `post` routes and controllers

--- controllers/departments.js
+++ controllers/departments.js
@@ -1,6 +1,39 @@
 const db = require("../database/db2");
 const Department = require('../models/Department')
 
+module.exports.createDepartment = async (req, res) => {
+    try {
+        if (!req.body.departmentNumber) throw new Error("Department number is required");
+        if (!req.body.name) throw new Error("A Department name is required");
+        if (!req.body.managerNumber) throw new Error("A manager number is required");
+        if (!req.body.reportingDepartment) throw new Error("A reporting department is required");
+        if (!req.body.location) throw new Error("A location is required");
+
+        await db.query(
+            `insert into sample.department (
+                deptno,
+                deptname,
+                mgrno,
+                admrdept,
+                location
+                )
+            values (
+                '${req.body.departmentNumber}',
+                '${req.body.name}',
+                '${req.body.managerNumber}',
+                '${req.body.reportingDepartment}',
+                '${req.body.location}'
+                )`
+        );
+
+    } catch (err) {
+        return res.status(500).send({
+            message: err.message,
+        });
+    }
+    return res.json(req.body);
+};
+
 module.exports.getAllDepartments = async (req, res) => {
     try {
         const resultSet = await db.query(`select * from sample.department`);
@@ -19,7 +52,7 @@ module.exports.getDepartment = async (req, res) => {
     try {
         const departmentNumber = req.params.id;
         const resultSet = await db.query(`select * from sample.department where deptno = '${departmentNumber}'`);
-        
+
         if (resultSet.length === 0) {
             return res.status(404).send({
                 message: `Department with departmentNumber ${departmentNumber} not found.`,
--- routes/employees.js
+++ routes/employees.js
@@ -2,8 +2,8 @@ const express = require(`express`);
 const router = express.Router();
 const EmployeeController = require("../controllers/employees");
 
+router.post("/", EmployeeController.createEmployee);
 router.get("/", EmployeeController.getAllEmployees);
 router.get("/:id", EmployeeController.getEmployee);
-//router.delete("/:id", EmployeeController.deleteEmployee);
 
 module.exports = router;

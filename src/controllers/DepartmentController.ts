ae909ea79871ed89e7b4dd7a8520413e3d70d0d8
Cole Maxwell <cole.maxwell@deltadentalsd.com>
2022-09-13 18:52:49 -0500

    Fix connection pool release

--- src/controllers/DepartmentController.ts
+++ src/controllers/DepartmentController.ts
@@ -5,8 +5,7 @@ export class DepartmentController {
   //Get all departments
   public static async getAllDepartments(req: any, res: any) {
     try {
-      const db2 = await db2ConnectionPool.connect();
-      const result = await db2.query(
+      const result = await db2ConnectionPool.query(
         "SELECT * FROM sample.department"
       );
       const departments = result.map((row: any) => new Department(row));
@@ -19,8 +18,7 @@ export class DepartmentController {
   //Get a single department by department number
   public static async getDepartmentByNumber(req: any, res: any) {
     try {
-      const db2 = await db2ConnectionPool.connect();
-      const result = await db2.query(
+      const result = await db2ConnectionPool.query(
         "SELECT * FROM sample.department WHERE DEPTNO = ?",
         [req.params.departmentNumber]
       );
@@ -34,8 +32,7 @@ export class DepartmentController {
   //Create a new department
   async createDepartment(req: any, res: any) {
     try {
-      const db2 = await db2ConnectionPool.connect();
-      const result = await db2.query(
+      const result = await db2ConnectionPool.query(
         "INSERT INTO sample.department (DEPTNO, DEPTNAME, MGRNO, ADMRDEPT, LOCATION) VALUES (?, ?, ?, ?, ?)",
         [
           req.body.departmentNumber,
@@ -51,68 +48,3 @@ export class DepartmentController {
     }
   }
 }
-
-
-/* module.exports.createDepartment = async (req: Request, res: Response) => {
-    try {
-
-        await db2ConnectionPool.query(
-            `insert into sample.department (
-                deptno,
-                deptname,
-                mgrno,
-                admrdept,
-                location
-                )
-            values (
-                '${req.body.departmentNumber}',
-                '${req.body.name}',
-                '${req.body.managerNumber}',
-                '${req.body.reportingDepartment}',
-                '${req.body.location}'
-                )`
-        );
-
-    } catch (err: OdbcError) {
-        return res.status(500).send({
-            message: err.message,
-        });
-    }
-    return res.json(req.body);
-}; */
-
-/* module.exports.getAllDepartments = async (req: Request, res: Response) => {
-    try {
-        const resultSet = await db2ConnectionPool.query(`select * from sample.department`);
-        var departments: Array<Department> = [];
-        resultSet.forEach(dept => departments.push(new Department(dept)));
-
-    } catch (err) {
-        return res.status(500).send({
-            message: err.message,
-        });
-    }
-    return res.json(departments);
-}; */
-
-/* module.exports.getDepartment = async (req: Request, res: Response) => {
-    try {
-        const departmentNumber = req.params.id;
-        const resultSet = await db.query(`select * from sample.department where deptno = '${departmentNumber}'`);
-
-        if (resultSet.length === 0) {
-            return res.status(404).send({
-                message: `Department with departmentNumber ${departmentNumber} not found.`,
-            });
-        } else if (resultSet.length === 1) {
-            var department = new Department(resultSet[0]);
-        }
-
-    } catch (err) {
-        return res.status(500).send({
-            message: err.message,
-        });
-    }
-
-    return res.json(department);
-}; */
